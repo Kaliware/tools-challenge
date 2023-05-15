959122ebe40abcc99d76dca39b791593a1ff69e6
Lucas Lourenço <kaliware.player@gmail.com>
2023-05-15 03:32:28 -0300

    Created custom exceptions

---
+++ src/main/java/com/kaliware/payment/toolschallenge/exceptions/ParsingException.java
@@ -0,0 +1,7 @@
+package com.kaliware.payment.toolschallenge.exceptions;
+
+public class ParsingException extends RuntimeException{
+  public ParsingException(String message){
+    super(message);
+  }
+}
